a53eb7a6852d73e5917d44f3c69b0d86abfecf87
Ervin <evgnz24@gmail.com>
2023-05-06 15:31:14 -0500

    Started working on gameboard class, This class will be used for when game starts, each player can place and attack opponents ship

--- src/main.js
+++ src/main.js
@@ -1,3 +1,36 @@
-Class GameBoard {
-  
+class GameBoard {
+  #board = [];
+
+  #boardLength = 10;
+
+  constructor() {
+    this.createBoard();
+  }
+
+  /**
+   * @x coordinate for x
+   * @y coordinate for y
+   * @func check if ship us hit or not,
+   * if it hot then it should send the
+   * hit function to the right ship.
+   * Otherwise record the missed shot
+   */
+  //   recieveAttack(x, y) {}
+  createBoard() {
+    // loop for creating the rows 'x'
+    for (let i = 0; i < this.#boardLength; i++) {
+      this.#board[i] = [];
+    }
+
+    // loops populates the rows 'y'
+    for (let n = 0; n < this.#boardLength; n++) {
+      for (let j = 0; j < this.#boardLength; j++) {
+        this.#board[n][j] = {
+          x: n,
+          y: j,
+          value: 0,
+        };
+      }
+    }
+  }
 }
